41fe7eb95bcb18da15722a26ab958d5a8637b711
oborys <oborysen@e1r2p17.unit.ua>
2018-05-24 18:18:01 +0300

    check Back State

---
+++ src/object/checkBackState.py
@@ -0,0 +1,20 @@
+#
+
+def checkBackState(cubFace, color):
+	if cubFace[1][1] == color:
+		return (1, 0)
+	elif cubFace[0][1] == color and cubFace[1][1] == color and cubFace[1][2] == color:
+		return (2, 0)
+	elif cubFace[1][0] == color and cubFace[1][1] == color and cubFace[0][1] == color:
+		return (2, 1)
+	elif cubFace[2][1] == color and cubFace[1][1] == color and cubFace[1][0] == color:
+		return (2, 2)
+	elif cubFace[1][2] == color and cubFace[1][1] == color and cubFace[2][1] == color:
+		return (2, 3)
+	elif cubFace[1][0] == color and cubFace[1][1] == color and cubFace[1][2] == color:
+		return (3, 0)
+	elif cubFace[0][1] == color and cubFace[1][1] == color and cubFace[2][1] == color:
+		return (3, 1)
+	elif cubFace[0][1] == color and cubFace[1][1] == color and cubFace[1][2] == color and cubFace[1][0] == color and cubFace[2][1] == color:
+		return (4, 0)
+	return False
