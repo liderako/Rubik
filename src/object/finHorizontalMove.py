5bf751b069514cbf0ddf85ac2c33bf329d31b847
oborys <oborysen@e1r2p17.unit.ua>
2018-05-24 19:14:19 +0300

    checkBackState

---
+++ src/object/finHorizontalMove.py
@@ -0,0 +1,15 @@
+
+import sys
+from src.object.Cubik import Cubik
+from src.object.CheckerColors import *
+
+def finHorizontalMove(cubOrigin, cubCurrent, moveList):
+	checkerManager = CheckerColors()
+
+
+#for test
+cub = Cub(3)
+
+cubmove = Cub(3)
+
+
