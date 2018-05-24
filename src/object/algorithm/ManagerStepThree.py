a9852614c734e81ac34a6897729dd6145f45e21e
Anton Sviridov <asvirido@e1r6p1.unit.ua>
2018-05-24 17:02:56 +0300

    three step done

--- src/object/algorithm/ManagerStepThree.py
+++ src/object/algorithm/ManagerStepThree.py
@@ -13,10 +13,6 @@ class ManagerStepThree:
 		self.checkerManager = CheckerColors()
 	
 	def run(self, cubCurrent, solveMoveList):
-		print ("______________________START___________________________") #################################################### delelte delete line
-		cubCurrent.printCubik() #################################################### delelte delete line
-		print ("______________________________________________________") #################################################### delelte delete line
-
 		if ((self.finishedThreeColorPosition(cubCurrent, ["green", "orange"])) == False):
 			self.moving(cubCurrent, solveMoveList, ["green", "orange"], "front")
 		
@@ -25,18 +21,9 @@ class ManagerStepThree:
 		
 		if ((self.finishedThreeColorPosition(cubCurrent, ["blue", "red"])) == False):
 			self.moving(cubCurrent, solveMoveList, ["blue", "red"], "back")
-		# 
 		if ((self.finishedThreeColorPosition(cubCurrent, ["red", "green"])) == False):
 			self.moving(cubCurrent, solveMoveList, ["red", "green"], "right")
-		print ("______________________END___________________________") #################################################### delelte delete line
-		cubCurrent.printCubik() #################################################### delelte delete line
-		print ("Solve Move") ######################################################
-		for x in solveMoveList: ######################################################
-			print (x, end=" ") ############################# #########################
-		print("") ######################################################
-		######################################################
-		print ("______________________________________________________") #################################################### delelte delete line
-	
+		cubCurrent.printCubik()
 	def finishedThreeColorPosition(self, cubCurrent, colorsList):
 		return checkPositionColor(self.cubOrigin, cubCurrent, colorsList[0], colorsList[1])
 
@@ -51,15 +38,44 @@ class ManagerStepThree:
 		
 		if (checkSideList[0] == True):
 			self.moveToTryPosition(cubCurrent, solveMoveList, colorsList)
-			print ("CheckSide true")
-			print ("ColorsList", colorsList)
-			print ("End")
-		# if (self.listPositionCubCurrent[0][0] == "upper"):
-			# self.moveToCente(cubCurrent, solveMoveList, colorsList, face)
+		else:
+			if (self.listPositionCubCurrent[0][0] != "down"):
+				self.pushDown(cubCurrent, solveMoveList, colorsList)
+				self.listPositionCubCurrent = self.updatePositionList(cubCurrent, colorsList)
+			if (self.listPositionCubCurrent[0][0] != "down"):
+				print ("ERROR DOWN IS NOT")
+				sys.exit(-1)
+			self.listPositionCubCurrent = self.updatePositionList(cubCurrent, colorsList)
+			if (self.listPositionCubCurrent[0][0] == "down"):
+				self.moveToCente(cubCurrent, solveMoveList, colorsList, face)
+				self.listPositionCubCurrent = self.updatePositionList(cubCurrent, colorsList)
+				self.moveToTryPosition(cubCurrent, solveMoveList, colorsList)
+	def 	moveToCente(self, cubCurrent, solveMoveList, colorsList, face):
+		checkSideList = self.checkSide(cubCurrent, colorsList)
+		while (checkSideList[0] == False):
+			cubCurrent.moveD()
+			solveMoveList.append("D")
+			checkSideList = self.checkSide(cubCurrent, colorsList)
 
-	# def 	moveToCente(self, cubCurrent, solveMoveList, colorsList, face):
-	# 	# while ()
-	# 	pass
+	def 	pushDown(self, cubCurrent, solveMoveList, colorsList):
+		faceOne, faceTwo, colorOne, colorTwo = self.getSideParams(cubCurrent, colorsList)
+		pattern = self.getPatternPushDown(faceOne, faceTwo)
+		if (pattern == "rightPattern"):
+			self.moveFormulaRight(cubCurrent, solveMoveList, faceOne)
+		elif (pattern == "leftPattern"):
+			self.moveFormulaLeft(cubCurrent, solveMoveList, faceOne)
+
+	def 	getPatternPushDown(self, faceOne, faceTwo):
+		if (faceOne == "left"):
+			if (faceTwo == "front"):
+				return ("rightPattern")
+			elif (faceTwo == "back"):
+				return ("leftPattern")
+
+		elif (faceOne == "front" and faceTwo == "right"):
+				return ("rightPattern")
+		elif (faceOne == "right" and faceTwo == "back"):
+			return ("rightPattern")
 
 	def 	getSideParams(self, cubCurrent, colorsList):
 		self.listPositionCubCurrent = self.updatePositionList(cubCurrent, colorsList)
@@ -73,25 +89,21 @@ class ManagerStepThree:
 		down, face, colorDown, colorFace = self.getSideParams(cubCurrent, colorsList)
 		if (down != "down"):
 			return [False, "null"]
-		
 		if (face == "front"):
 			if (colorDown == "orange" and colorFace == "green"):
 				return [True, "leftPattern"] 
 			elif (colorDown == "red" and colorFace == "green"):
 				return [True, "rightPattern"] 
-		
 		if (face == "back"):
 			if (colorDown == "orange" and colorFace == "blue"):
 				return [True, "leftPattern"] 
 			elif (colorDown == "red" and colorFace == "blue"):
 				return [True, "rightPattern"]
-		
 		if (face == "right"):
 			if (colorDown == "blue" and colorFace == "red"):
 				return [True, "rightPattern"]
 			elif (colorDown == "green" and colorFace == "red"):
 				return [True, "leftPattern"] 
-		
 		if (face == "left"):
 			if (colorDown == "green" and colorFace == "orange"):
 				return [True, "rightPattern"] 
@@ -102,39 +114,33 @@ class ManagerStepThree:
 	def 	moveToTryPosition(self, cubCurrent, solveMoveList, colorsList):
 		down, face, colorDown, colorFace = self.getSideParams(cubCurrent, colorsList)
 		checkSideList = self.checkSide(cubCurrent, colorsList)
-		print ("PATTERN ", checkSideList[1])
 		if (checkSideList[1] == "rightPattern"):
 			self.moveFormulaRight(cubCurrent, solveMoveList, face)
 		elif (checkSideList[1] == "leftPattern"):
 			self.moveFormulaLeft(cubCurrent, solveMoveList, face)
-		else: #################################################### delelte delete line
-			print ("WTF??! move to try position") #################################################### delelte delete line
-			sys.exit(-1) #################################################### delelte delete line
 
 	def 	moveFormulaLeft(self, cubCurrent, solveMoveList, face):
 		mixManager = MixManager()
 
-		if (face == "front"): # [ "D", "L", "D'", "L'", "D'", "F'", "D", "F" ]
+		if (face == "front"):
 			mixManager.mixRun([ "D", "L", "D'", "L'", "D'", "F'", "D", "F" ], cubCurrent)
 			appendListInList(solveMoveList, [ "D", "L", "D'", "L'", "D'", "F'", "D", "F" ])
-		elif (face == "left"): # [ "D", "B", "D'", "B'", "D'", "L'", "D", "L" ]
+		elif (face == "left"):
 			mixManager.mixRun([ "D", "B", "D'", "B'", "D'", "L'", "D", "L" ], cubCurrent)
 			appendListInList(solveMoveList, [ "D", "B", "D'", "B'", "D'", "L'", "D", "L" ])
-			print ("FACE left- RIGHT BUG")
-		elif (face == "back"): # D' L' D L D B D' B'
+		elif (face == "back"):
 			mixManager.mixRun([ "D'", "L'", "D", "L", "D", "B", "D'", "B'" ], cubCurrent)
 			appendListInList(solveMoveList, [ "D'", "L'", "D", "L", "D", "B", "D'", "B'" ])
-		elif (face == "right"): # [ "D", "F", "D'", "F'", "D'", "R'", "D", "R" ]
+		elif (face == "right"):
 			mixManager.mixRun([ "D", "F", "D'", "F'", "D'", "R'", "D", "R" ], cubCurrent)
 			appendListInList(solveMoveList, [ "D", "F", "D'", "F'", "D'", "R'", "D", "R" ])
 
 	def 	moveFormulaRight(self, cubCurrent, solveMoveList, face):
 		mixManager = MixManager()
-		if (face == "front"): # [ "D'", "R'", "D", "R", "D", "F", "D'", "F'" ]
+		if (face == "front"):
 			mixManager.mixRun([ "D'", "R'", "D", "R", "D", "F", "D'", "F'" ], cubCurrent)
 			appendListInList(solveMoveList, [ "D'", "R'", "D", "R", "D", "F", "D'", "F'" ])
-		elif (face == "left"): # [ "D'", "F'", "D", "F", "D", "L", "D'", "L'" ]
-			print ("FACE left- RIGHT BUG")
+		elif (face == "left"):
 			mixManager.mixRun([ "D'", "F'", "D", "F", "D", "L", "D'", "L'" ], cubCurrent)
 			appendListInList(solveMoveList, [ "D'", "F'", "D", "F", "D", "L", "D'", "L'" ])
 		elif (face == "right"): # [ "D'", "B'", "D", "B", "D", "R", "D'", "R'"]
