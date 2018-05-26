3361c1f1618bbe9fe2d88e66a97f3e103b1f72d9
oborys <borysenko.oleksii@gmail.com>
2018-05-26 16:13:13 +0300

    Update finHorizontalMove.py
    add cubik move

--- src/object/finHorizontalMove.py
+++ src/object/finHorizontalMove.py
@@ -10,70 +10,99 @@ def finHorizontalMove(cubOrigin, cubCurrent, moveList):
 	while (i == 0):
 		if cubCurrent.front[0][2] == 'orange' and cubCurrent.front[1][2] == 'orange' and cubCurrent.front[2][2] == 'green':
 			moveList.append("U")
+			cubmove.moveU()
 			break
 		if cubCurrent.front[0][2] == 'red' and cubCurrent.front[1][2] == 'red' and cubCurrent.front[2][2] == 'green':
 			moveList.append("D")
+			cubmove.moveD()
 			break
 		if cubCurrent.front[0][2] == 'blue' and cubCurrent.front[1][2] == 'blue' and cubCurrent.front[2][2] == 'green':
 			moveList.append("D2")
+			cubmove.moveDoubleD()
 			break
 		if cubCurrent.front[0][2] == 'orange':
 			moveList.append("U")
+			cubmove.moveU()
 		elif cubCurrent.front[0][2] == 'red':
 			moveList.append("U'")
+			cubmove.moveBackU()
 		elif cubCurrent.front[0][2] == 'blue':
 			moveList.append("U'")
 			moveList.append("U'")
+			cubmove.moveBackU()
+			cubmove.moveBackU()
 		if cubCurrent.front[1][2] == 'orange' and cubCurrent.front[2][2] == 'green':
 			moveList.append("D")
 			moveList.append("U'")
+			cubmove.moveD()
+			cubmove.moveBackU()
 			break
 		if cubCurrent.front[1][2] == 'red' and cubCurrent.front[2][2] == 'green':
 			moveList.append("D'")
 			moveList.append("U")
+			cubmove.moveBackD()
+			cubmove.moveU()
 			break
 		if cubCurrent.front[1][2] == 'blue' and cubCurrent.front[2][2] == 'green':
 			moveList.append("U2")
 			moveList.append("D'")
 			moveList.append("D'")
+			cubmove.moveDoubleU()
+			cubmove.moveBackD()
+			cubmove.moveBackD()
 			break
 		if cubCurrent.front[1][2] == 'orange' and cubCurrent.front[2][2] == 'red':
 			moveList.append("U'")
 			moveList.append("D2")
+			cubmove.moveBackU()
+			cubmove.moveDoubleD()
 			break
 		if cubCurrent.front[1][2] == 'orange' and cubCurrent.front[2][2] == 'blue':
 			moveList.append("D'")
 			moveList.append("D'")
+			cubmove.moveBackD()
+			cubmove.moveBackD()
 			break
 		if cubCurrent.front[1][2] == 'orange' and cubCurrent.front[2][2] == 'orange':
 			moveList.append("U'")
+			cubmove.moveBackU()
 			break
 		if cubCurrent.front[1][2] == 'red' and cubCurrent.front[2][2] == 'blue':
-			moveList.append("D")
-			moveList.append("D")
+			moveList.append("D2")
+			cubmove.moveDoubleD()
 			break
 		if cubCurrent.front[1][2] == 'red' and cubCurrent.front[2][2] == 'orange':
 			moveList.append("U")
 			moveList.append("D2")
+			cubmove.moveU()
+			cubmove.moveDoubleD()
 			break
 		if cubCurrent.front[1][2] == 'red' and cubCurrent.front[2][2] == 'red':
 			moveList.append("U")
+			cubmove.moveU()
 			break
 		if cubCurrent.front[1][2] == 'blue' and cubCurrent.front[2][2] == 'orange':
 			moveList.append("U")
+			cubmove.moveU()
 			break
 		if cubCurrent.front[1][2] == 'blue' and cubCurrent.front[2][2] == 'red':
 			moveList.append("U2", "D'")
+			cubmove.moveDoubleU()
+			cubmove.moveD()
 			break
 		if cubCurrent.front[1][2] == 'blue' and cubCurrent.front[2][2] == 'blue':
 			moveList.append("U2")
+			cubmove.moveDoubleU()
 			break
 		if cubCurrent.front[2][2] == 'orange':
 			moveList.append("D'")
+			cubmove.moveBackD()
 		if cubCurrent.front[2][2] == 'red':
 			moveList.append("D")
+			cubmove.moveD()
 		if cubCurrent.front[2][2] == 'blue':
 			moveList.append("D2")
+			cubmove.moveDoubleD()
 		i += 1
 
 	return moveList
