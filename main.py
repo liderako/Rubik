dd41a4f2ef82e96b6802683c55904d54e9cc8d09
Anton Sviridov <asvirido@e1r6p1.unit.ua>
2018-05-27 20:27:52 +0300

    update main

--- main.py
+++ main.py
@@ -8,19 +8,35 @@ from src.object.CheckerColors import CheckerColors
 from src.object.algorithm.Algorithm import *
 from src.printSolution import *
 
-stringUsage = "Usage: main.py [-i or -f] [count i or fileName]"
-if (len(sys.argv) != 3):
+stringUsage = "Usage: main.py String or [-i or -f or --help or -h] [count i or fileName] [-g or -gwt]"
+
+if (len(sys.argv) == 2 and (sys.argv[1] == "--help" or sys.argv[1] == "-h")):
+    print ("    Availabe move [ F B R L U D ]")
+    print ("    Modificator [ ' ] and [ 2 ]")
+    print ("    Example: F2 B' U2")
+    print ("    -i. This is random generator Mix moving.\n    Example: python main.py -f 25 -g")
+    print ("    -f. This is read from file.\n    Example: python main.py -f fileName -g")
+    print ("    -g. This is color print Solution.\n    Example: python main.py -i 25 -g")
+    print ("    -gt. This is withor print Solution.\n    Example: python main.py -f fileName -gwt")
+    print ("    -wc. This is withor color.\n    Example: python main.py -f fileName -wc")
+    sys.exit(-1)
+
+if (len(sys.argv) != 4 and len(sys.argv) != 2):
     errorExit(stringUsage)
-elif (sys.argv[1] != "-f" and sys.argv[1] != "-i"):
+if (len(sys.argv) == 4 and ((sys.argv[1] != "-f" and sys.argv[1] != "-i") or (sys.argv[3] != "-g" and sys.argv[3] != "-gt" and sys.argv[3] != "-wc"))):
     errorExit(stringUsage)
 
-if (sys.argv[1] == "-f"):
+if ((len(sys.argv) == 4) and sys.argv[1] == "-f"):
     readBuffer = readFile(sys.argv[2])
     validationManager = ValidationManager(readBuffer)
     validationManager.run()
+elif (len(sys.argv) == 2):
+    readBuffer = sys.argv[1]
+    validationManager = ValidationManager(sys.argv[1])
+    validationManager.run()
 mixManager = MixManager()
 moveList = list()
-if (sys.argv[1] == "-i"):
+if ((len(sys.argv) == 4) and sys.argv[1] == "-i"):
     try:
         i = int(sys.argv[2])
         if (i <= 0):
@@ -33,14 +49,25 @@ else:
 cub = Cubik(3)
 mixManager.mixRun(moveList, cub)
 
-print ("MixMoveList")
-for x in moveList:
-    print (x, end=" ")
-print ("Mix state cubik")
-cub.printCubik()
+if (len(sys.argv) == 4 and sys.argv[3] != "-wc"):
+    print ("Mix MoveList")
+    for x in moveList:
+        print (x, end=" ")
+    print ("")
+    print ("Mix state cubik")
+    cub.printCubik()
 
 algorithm = Algorithm(cub)
 cubOrigin = Cubik(3)
 mixManager.mixRun(moveList, cubOrigin)
-printSolution(cubOrigin, algorithm.run())
-
+if (len(sys.argv) == 4 and sys.argv[3] != "-wc"):
+    printSolution(cubOrigin, algorithm.run(), sys.argv[3])
+else:
+    solution = algorithm.run()
+    i = 0
+    for x in solution:
+        if (i != 0 and i != len(solution)):
+            print (end=" ")
+        print (x, end="")
+        i += 1
+    print ("")
--- src/printSolution.py
+++ src/printSolution.py
@@ -1,46 +1,68 @@
 import sys
 from src.object.Cubik import *
 
-def printSolution(cub, moveList):
+def printSolution(cub, moveList, flag):
+    cost = 0
     for i, move in enumerate(moveList):
         if move == 'U':
             cub.moveU()
+            cost += 1
         elif move == 'D':
             cub.moveD()
+            cost += 1
         elif move == 'R':
             cub.moveR()
+            cost += 1
         elif move == 'L':
             cub.moveL()
+            cost += 1
         elif move == 'F':
             cub.moveF()
+            cost += 1
         elif move == 'B':
             cub.moveB()
-#
+            cost += 1
+
         elif move == "U'":
             cub.moveBackU()
+            cost += 1
         elif move == "D'":
             cub.moveBackD()
+            cost += 1
         elif move == "R'":
             cub.moveBackR()
+            cost += 1
         elif move == "L'":
             cub.moveBackL()
+            cost += 1
         elif move == "F'":
             cub.moveBackF()
+            cost += 1
         elif move == "B'":
             cub.moveBackB()
+            cost += 1
 #
         elif move == 'U2':
             cub.moveDoubleU()
+            cost += 1
         elif move == 'D2':
             cub.moveDoubleD()
+            cost += 1
         elif move == 'R2':
             cub.moveDoubleR()
+            cost += 1
         elif move == 'L2':
             cub.moveDoubleL()
+            cost += 1
         elif move == 'F2':
             cub.moveDoubleF()
+            cost += 1
         elif move == 'B2':
             cub.moveDoubleB()
+            cost += 1
 
         print ("Move number ", i, "move ", move)
-        cub.printCubik()
+        if (flag == "-g"):
+            cub.printCubik()
+        else:
+            cub.printCubikText()
