b1a16073aa827c79f172e5983dbea05d0306e1be
oborys <borysenko.oleksii@gmail.com>
2018-05-29 21:59:05 +0300

    Update optimizationMove.py
    added optimization for two similar move

--- src/optimizationMove.py
+++ src/optimizationMove.py
@@ -34,6 +34,21 @@ def optimizationMove(solution):
                     newlst.append("D")
                 f = 1
                 i += 2
+            elif (solution[i] == solution[i + 1]):
+                if solution[i] == "F" or solution[i] == "F'":
+                    newlst.append("F2")
+                elif solution[i] == "R" or solution[i] == "R'":
+                    newlst.append("R2")
+                elif solution[i] == "U" or solution[i] == "U'":
+                    newlst.append("U2")
+                elif solution[i] == "B" or solution[i] == "B'":
+                    newlst.append("B2")
+                elif solution[i] == "L" or solution[i] == "L'":
+                    newlst.append("L2")
+                elif solution[i] == "D" or solution[i] == "D'":
+                    newlst.append("D2")
+                f = 1
+                i += 1
         if f == 0:
             newlst.append(solution[i])
         i += 1
